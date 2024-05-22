057b34b84e5bdd3292284781f3dcaeb476e48b8e
DeDTihoN <t.grishechkin@gmail.com>
2024-05-22 22:14:15 +0300

    added long primes

--- Program.cs
+++ Program.cs
@@ -2,17 +2,20 @@
 using Crypto_Lab1_;
 using System;
 using System.Numerics;
+using System.Text;
 
 public class Program
 {
     public static void Main()
     {
-        for (int i=2;i<=30;++i){
-            Console.WriteLine("Generated binary with length "+i+": "+ToBase2(Ariphmetic.GetRandBinaryByLength(i)));
+        for (int i = 2; i <= 30; ++i)
+        {
+            Console.WriteLine("Generated binary with length " + i + ": " + ToBase2(Ariphmetic.GetRandBinaryByLength(i)));
         }
 
-        for (int i=2;i<=30;++i){
-            Console.WriteLine("Generated decimal with length "+i+": "+Ariphmetic.GeneratePrime(i));
+        for (int i = 2; i <= 30; ++i)
+        {
+            Console.WriteLine("Generated decimal with length " + i + ": " + Ariphmetic.GeneratePrime(i));
         }
 
         List<string> carlMichaelList = new List<string>
@@ -23,6 +26,16 @@ public class Program
 
         BigInteger[] strongLucasPseudoprimes = { 5459, 5777, 10877, 16109, 18971, 22499, 24569, 25199, 40309, 58519, 75077, 97439, 100127, 113573, 115639, 130139, 155819, 158399, 161027, 162133, 176399, 176471, 189419, 192509, 197801, 224369, 230691, 231703, 243629, 253259, 268349, 288919, 313499, 324899 };
         BigInteger[] strongPseudoprimesToBase2 = { 2047, 3277, 4033, 4681, 8321, 15841, 29341, 42799, 49141, 52633, 65281, 74665, 80581, 85489, 88357, 90751, 104653, 130561, 196093, 220729, 233017, 252601, 253241, 256999, 271951, 280601, 314821, 357761, 390937, 458989, 476971, 486737 };
+        BigInteger[] longPrimesNumbers = {
+            BigInteger.Parse("874976257858102471576213669489"),
+            BigInteger.Parse("7555554555554555554555554555557"),
+            BigInteger.Parse("101088811181811181118181118880101"),
+            BigInteger.Parse("983531983579983617983777983791983819"),
+            BigInteger.Parse("502360950888298027355518043327124471675959709617316205938938264006428765255489"),
+            BigInteger.Parse("227432689108589532754984915075774848386671439568260420754414940780761245893"),
+            BigInteger.Parse("95939187858177756965635755514945393533272521159"),
+            BigInteger.Parse("384956219213331276939737002152967117209600000001")
+        };
 
         for (int i = 0; i < carlMichaelList.Count; i++)
         {
@@ -45,6 +58,13 @@ public class Program
             if (Ariphmetic.MillerRabinPrimalityTest(strongPseudoprimesToBase2[i]))
             {
                 Console.WriteLine("Failed for base 2: " + strongPseudoprimesToBase2[i]);
+            }
+        }
+
+        for (int i = 0; i < longPrimesNumbers.Length; i++){
+            if (!Ariphmetic.BailliePSWPrimalityTest(longPrimesNumbers[i]))
+            {
+                Console.WriteLine("Failed for long prime: " + longPrimesNumbers[i]);
             }
         }
 
@@ -78,16 +98,16 @@ public class Program
     {
         byte[] byteArray = number.ToByteArray();
         string res = "";
-        res+="ByteArray: [";
+        res += "ByteArray: [";
         for (int i = 0; i < byteArray.Length; i++)
         {
-            res+=byteArray[i].ToString("X");
+            res += byteArray[i].ToString("X");
             if (i < byteArray.Length - 1)
             {
-                res+=", ";
+                res += ", ";
             }
         }
-        res+="]";
+        res += "]";
         return res;
     }
 }
